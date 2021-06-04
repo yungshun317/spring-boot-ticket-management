b046c64862eeceea4fd7bb0eb19000a558585dea
yungshun317 <seraph317@gmail.com>
2021-06-04 17:49:10 +0800

    [CHORE] Intialize a Spring Boot project

---
+++ src/main/java/yungshun/chang/springbootticketmanagement/SpringBootTicketManagementApplication.java
@@ -0,0 +1,13 @@
+package yungshun.chang.springbootticketmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTicketManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTicketManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yungshun/chang/springbootticketmanagement/SpringBootTicketManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package yungshun.chang.springbootticketmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTicketManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
