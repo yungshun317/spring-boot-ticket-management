6461546957d9bd0074eebe711daf9b84501f6cd4
yungshun317 <seraph317@gmail.com>
2021-06-12 16:18:09 +0800

    [FEAT] Create `HomeController` with a simple `map` as a result

--- src/main/java/yungshun/chang/springbootticketmanagement/SpringBootTicketManagementApplication.java
+++ src/main/java/yungshun/chang/springbootticketmanagement/SpringBootTicketManagementApplication.java
@@ -2,7 +2,9 @@ package yungshun.chang.springbootticketmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("yungshun.chang")
 @SpringBootApplication
 public class SpringBootTicketManagementApplication {
 
---
+++ src/main/java/yungshun/chang/springbootticketmanagement/controller/HomeController.java
@@ -0,0 +1,19 @@
+package yungshun.chang.springbootticketmanagement.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class HomeController {
+    @ResponseBody
+    @RequestMapping("")
+    public Map<String, Object> test() {
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("result", "Aloha");
+        return map;
+    }
+}
